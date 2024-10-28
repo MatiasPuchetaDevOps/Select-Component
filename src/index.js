6b4fbd3685d98348aeaa60770c3e12e57e7a4e9f
Maty <147735180+MatiasPuchetaDevOps@users.noreply.github.com>
2024-10-28 07:17:10 -0300

    npm update

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 
 export { default as SelectComponent } from '../src/Select/SelectComponent.jsx';
+export { default as ControllerSelectComponent } from '../src/Select/FormatChildrens/ControllerSelectComponent.jsx';
+export { useClearSelect } from '../src/context/Select/ClearSelectContext.jsx';
